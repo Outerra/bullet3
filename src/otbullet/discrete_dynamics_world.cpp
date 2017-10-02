2e7c04d2604637fe0e3060b918eadbaee6745d89
Brano Kemen <cameni@outerra.com>
2017-10-02 16:32:00 +0200

    comm update

--- src/otbullet/discrete_dynamics_world.cpp
+++ src/otbullet/discrete_dynamics_world.cpp
@@ -314,8 +314,7 @@ namespace ot {
 
                 get_obb(internal_obj_wrapper.getCollisionShape(), internal_obj_wrapper.getWorldTransform(), _from, _basis);
 
-                //if(!_sphere_intersect(_context, _from , _rad , _lod_dim, _triangles, _trees)) {
-                _relocation_offset = _tb_cache.get_array().ptr();
+                //_relocation_offset = _tb_cache.get_array().ptr();
                 
                 bool is_above_tm = false;
 
@@ -323,14 +322,14 @@ namespace ot {
                     DASSERT(_tree_batches.size() == 0);
                     continue;
                 }
-                
+/*                
                 if (_relocation_offset != _tb_cache.get_array().ptr()) {
                     coidlog_warning("discrete_dynamics_world", "Tree baches slot allocator rebased. Tree batches count: " << _tb_cache.count());
                     repair_tree_batches();
                     repair_tree_collision_pairs();
-                }
+                }*/
 
-                tri_count += _triangles.size();
+                tri_count += uint(_triangles.size());
 
 				if (_triangles.size() > 0) {
 
@@ -528,6 +527,32 @@ namespace ot {
         });
     }
 
+    void discrete_dynamics_world::get_obb(const btCollisionShape * cs,const btTransform& t, double3& cen, float3x3& basis)
+    {
+        btVector3 min, max;
+        cs->getAabb(btTransform::getIdentity(), min, max);
+        btVector3 bt_cen = (min + max) * btScalar(0.5);
+        btVector3 half = max - bt_cen;
+        bt_cen += t.getOrigin();
+        btMatrix3x3 bt_basis;
+        bt_basis[0] = t.getBasis() * btVector3(half[0], 0, 0);
+        bt_basis[1] = t.getBasis() * btVector3(0, half[1], 0);
+        bt_basis[2] = t.getBasis() * btVector3(0, 0, half[2]);
+
+        cen = double3( bt_cen[0], bt_cen[1], bt_cen[2] );
+        basis[0] = float3(bt_basis[0][0], bt_basis[0][1], bt_basis[0][2]);
+        basis[1] = float3(bt_basis[1][0], bt_basis[1][1], bt_basis[1][2]);
+        basis[2] = float3(bt_basis[2][0], bt_basis[2][1], bt_basis[2][2]);
+    }
+
+    void discrete_dynamics_world::oob_to_aabb(const btVector3 & src_cen, const btMatrix3x3 & src_basis, const btVector3 & dst_cen, const btMatrix3x3 & dst_basis, btVector3 & aabb_cen, btVector3 & aabb_half)
+    {
+        aabb_cen = src_cen - dst_cen;
+        aabb_half[0] = dst_basis[0].dot(src_basis[0]) + dst_basis[0].dot(src_basis[1]) + dst_basis[0].dot(src_basis[2]);
+        aabb_half[1] = dst_basis[1].dot(src_basis[0]) + dst_basis[0].dot(src_basis[1]) + dst_basis[0].dot(src_basis[2]);
+        aabb_half[2] = dst_basis[2].dot(src_basis[0]) + dst_basis[0].dot(src_basis[1]) + dst_basis[0].dot(src_basis[2]);
+    }
+/*
     void discrete_dynamics_world::repair_tree_collision_pairs()
     {
         _tree_collision_pairs.for_each([&](tree_collision_pair& tcp) {
@@ -550,7 +575,7 @@ namespace ot {
                 }
             }
         });
-    }
+    }*/
 
     bt::tree_collision_info * discrete_dynamics_world::get_tree_collision_info(const tree_collision_pair& tcp)
     {
@@ -645,7 +670,7 @@ namespace ot {
         , _debug_terrain_triangles(1024)
         , _debug_trees(1024)
         , _tb_cache(1024)
-        , _relocation_offset(0)
+        //, _relocation_offset(0)
 	{
 		btTriangleShape * ts = new btTriangleShape();
 		ts->setMargin(0.0f);
