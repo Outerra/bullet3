8da8625875c1d365d5e94942b6f53c599ce2840c
Cyril <cyril.gramblicka@gmail.com>
2020-10-06 13:58:10 +0200

    terrain collision filter added

--- src/otbullet/discrete_dynamics_world.cpp
+++ src/otbullet/discrete_dynamics_world.cpp
@@ -28,6 +28,7 @@
 #include <comm/profiler/profiler.h>
 
 #include <ot/glm/glm_ext.h>
+#include <ot/object_cfg.h>
 
 extern unsigned int gOuterraSimulationFrame;
 
@@ -510,15 +511,12 @@ namespace ot {
             btCollisionObject * obj = m_collisionObjects[i];
             btRigidBody * rb = btRigidBody::upcast(obj);
 
-        /*    btVector3 dbg_min, dbg_max;
-            rb->getCollisionShape()->getAabb(rb->getWorldTransform(), dbg_min, dbg_max);
-            add_debug_aabb(dbg_min, dbg_max, btVector3(1, 1, 1));
-
-*/
-            if (!rb || (obj->getCollisionShape()->getShapeType() != SPHERE_SHAPE_PROXYTYPE &&
+            if (!rb || 
+                (obj->getCollisionShape()->getShapeType() != SPHERE_SHAPE_PROXYTYPE &&
                 obj->getCollisionShape()->getShapeType() != CAPSULE_SHAPE_PROXYTYPE &&
                 !obj->getCollisionShape()->isConvex() &&
-                obj->getCollisionShape()->getShapeType() != COMPOUND_SHAPE_PROXYTYPE))
+                obj->getCollisionShape()->getShapeType() != COMPOUND_SHAPE_PROXYTYPE) ||
+                (rb->getBroadphaseHandle()->m_collisionFilterMask & ot::collision::cg_terrain) == 0)
             {
                 continue;
             }
