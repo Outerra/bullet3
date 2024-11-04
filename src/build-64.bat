63ec5dbc4e6e961e405c380bf3cd48e895711a6c
cyrilgramblicka <cyril.gramblicka@gmail.com>
2024-11-04 10:26:51 +0100

    build-64.bat fix (BUG: REVISION constant was bigger than int size and it caused compilation error)

--- src/build-64.bat
+++ src/build-64.bat
@@ -31,12 +31,14 @@ echo %userver%>otbullet\version.last
 
 rem rev number
 
-call git rev-parse --short HEAD >otbullet\revision.last
-set /p revision=<otbullet\revision.last
+call git rev-parse --short=8 HEAD >otbullet\revision.last
+set /p REVISION=<otbullet\revision.last
 
 echo Revision: %revision%
 
-set VERSTR=%userver%.%revision%
+set REVISION=x%REVISION%
+
+set VERSTR=%userver%
 set VERLST=%userver%
 set VERLST=%VERLST:.=,%
 
